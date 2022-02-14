0a3f1a2dbec6e07e1f3bbaeae6ea6d8186a8dac0
Deborah Caroline <deborahcaroline615@gmail.com>
2022-02-13 22:24:04 -0300

    aula 68 threads interface Runnable

---
+++ curso-java-intermediario/src/com/deborah/curso/intermediario/java/aula68/Teste.java
@@ -0,0 +1,18 @@
+package com.deborah.curso.intermediario.java.aula68;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		
+		MinhaThreadRunnable thread1 = new MinhaThreadRunnable("#1", 500);
+		
+//		Thread t1 = new Thread(thread1);
+//		t1.start();
+		
+		MinhaThreadRunnable thread2 = new MinhaThreadRunnable("#2", 100);
+		
+		MinhaThreadRunnable thread3 = new MinhaThreadRunnable("#3", 1000);
+	}
+
+}
