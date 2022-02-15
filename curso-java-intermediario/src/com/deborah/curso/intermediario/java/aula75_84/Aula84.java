ac9df81e7a23c9703386e1e9c3017933a70ecd2d
Deborah Caroline <deborahcaroline615@gmail.com>
2022-02-14 21:41:36 -0300

    aula 84 String doArquivo = 1

---
+++ curso-java-intermediario/src/com/deborah/curso/intermediario/java/aula75_84/Aula84.java
@@ -0,0 +1,20 @@
+package com.deborah.curso.intermediario.java.aula75_84;
+
+import java.util.StringTokenizer;
+
+public class Aula84 {
+
+	public static void main(String[] args) {
+
+		String doArquivo = "1;Deborah;30;";
+
+		StringTokenizer st = new StringTokenizer(doArquivo, ";");
+
+		while (st.hasMoreTokens()){
+			System.out.println(st.nextToken());
+		}
+
+
+	}
+
+}
