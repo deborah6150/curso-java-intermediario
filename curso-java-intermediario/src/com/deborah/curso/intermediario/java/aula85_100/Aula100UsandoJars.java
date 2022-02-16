61e832bb7dd607475822ebb5c69faa687f98921f
Deborah Caroline <deborahcaroline615@gmail.com>
2022-02-16 14:57:10 -0300

    aula 100 Instalando jars no Projeto

---
+++ curso-java-intermediario/src/com/deborah/curso/intermediario/java/aula85_100/Aula100UsandoJars.java
@@ -0,0 +1,12 @@
+package com.deborah.curso.intermediario.java.aula85_100;
+
+
+public class Aula100UsandoJars {
+
+	public static void main(String[] args) {
+		
+		System.out.println(StringUtils.capitalize("Deborah"));
+
+	}
+
+}
